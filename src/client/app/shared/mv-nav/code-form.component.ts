19ff0fb2540cb49122b2ba7d7131a39c4f6261ab
pengfei <270137794@qq.com>
2017-03-16 15:37:57 +0800

    弹窗样式问题

--- src/client/app/shared/mv-nav/code-form.component.ts
+++ src/client/app/shared/mv-nav/code-form.component.ts
@@ -5,6 +5,7 @@ import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   moduleId: module.id,
   selector: 'mv-code-form-content',
+  styleUrls:['mv-nav.component.css'],
   templateUrl: 'code-form.component.html'
 })
 export class MvCodeJsFormContent {
