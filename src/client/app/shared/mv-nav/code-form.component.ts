da9d817363b0a62250284af40527eebb20ed231a
hulb <laborc2009@gmail.com>
2017-03-17 16:34:38 +0800

    优化code

--- src/client/app/shared/mv-nav/code-form.component.ts
+++ src/client/app/shared/mv-nav/code-form.component.ts
@@ -1,18 +1,15 @@
-import { Component,Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   moduleId: module.id,
   selector: 'mv-code-form-content',
-  styleUrls:['mv-nav.component.css'],
+  styleUrls: ['mv-nav.component.css'],
   templateUrl: 'code-form.component.html'
 })
 export class MvCodeJsFormContent {
-  @Input() code:string;
-  codeSTr:string = this.code;
-  onChange(code:string) {
-  	this.codeSTr = code;
-  }
+  @Input() code: string;
   constructor(public activeModal: NgbActiveModal) { }
+
 }
